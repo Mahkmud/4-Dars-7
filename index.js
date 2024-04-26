206d35e82e9a0eabf904080d880800e75090431a
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-04-26 09:47:54 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,17 @@
+let makhmudjon  = ["Makhmudjon" , 14, true, "sag'bon"];
+
+console.log(makhmudjon);
+console.log(makhmudjon.length);
+
+let rost = confirm("Manziling'iz to'g'rimi?");
+
+if (rost){
+
+}else{
+    makhmudjon.pop();
+}
+
+console.log(makhmudjon);
+
+
+
